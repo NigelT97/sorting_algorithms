89961a84d5d78e10fa4ee667d94ea8c9ca75f493
PStar <nigelmj97@gmail.com>
2023-09-25 21:31:27 +0200

    delete unecessary

--- sort.h
+++ sort.h
@@ -19,17 +19,6 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
-/**
- * enum bool - Enumeration of Boolean values.
- * @false: Equals 0.
- * @true: Equals 1.
- */
-typedef enum bool
-{
-	false = 0,
-	true
-} bool;
-
 /* Printing helper functions */
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
